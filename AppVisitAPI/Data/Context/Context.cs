b2496066abee0b6c83b3f38b7b8633f14dc2318d
leonardo reis <leonardo.amorim@gomara.tech>
2023-09-22 20:32:53 -0300

    add connection string and context

---
+++ AppVisitAPI/Data/Context/Context.cs
@@ -0,0 +1,19 @@
+﻿using AppVisitAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppVisitAPI.Data.Context
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> opt) : base(opt)
+        {
+            
+        }
+
+        public DbSet<Arquivo> Arquivos { get; set; }
+        public DbSet<Cidade> Cidades { get; set; }
+        public DbSet<Estado> Estados { get; set; }
+        public DbSet<Lugar> Lugares { get; set; }
+        public DbSet<Pais> Paises { get; set; }
+    }
+}
--- AppVisitAPI/Program.cs
+++ AppVisitAPI/Program.cs
@@ -1,3 +1,6 @@
+using AppVisitAPI.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<Context>(x => x.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
