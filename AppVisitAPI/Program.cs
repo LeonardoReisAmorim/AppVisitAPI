67a542d3fbddb0df6ec992f903070866fcbffdb7
leonardo reis <leonardo.amorim@gomara.tech>
2024-04-11 00:34:26 -0300

    colocar novamente a url de dev para realizar o desenvolvimento

--- AppVisitAPI/Program.cs
+++ AppVisitAPI/Program.cs
@@ -12,9 +12,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//Utilizando connection string localhost para desenvolvimento. Quando estiver todo o desenvolvimento finalizado, gero uma nova imagem e subo no docker hub, e ai faço a aplicação rodar em docker 
-//builder.Services.AddDbContext<Context>(x => x.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("DevelopmentConnection")));
-builder.Services.AddDbContext<Context>(x => x.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//Utilizando connection string localhost para desenvolvimento.
+builder.Services.AddDbContext<Context>(x => x.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("DevelopmentConnection")));
+//builder.Services.AddDbContext<Context>(x => x.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<PaisService, PaisService>();
 builder.Services.AddScoped<EstadoService, EstadoService>();
