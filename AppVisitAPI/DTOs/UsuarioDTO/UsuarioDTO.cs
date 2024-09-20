ae0a3a1fdbe7dc84ff789afac5f5b9fd55dd362e
leonardo reis <leonardo.amorim@gomara.tech>
2024-09-19 22:30:39 -0300

    add isAdmin in usuarioDto

--- AppVisitAPI/DTOs/UsuarioDTO/UsuarioDTO.cs
+++ AppVisitAPI/DTOs/UsuarioDTO/UsuarioDTO.cs
@@ -8,6 +8,7 @@ namespace AppVisitAPI.DTOs.UsuarioDTO
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
+        public bool IsAdmin { get; set; } = false;
         [NotMapped]
         public string Password { get; set; }
     }
